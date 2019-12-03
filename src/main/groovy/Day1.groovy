3594a37b81e58169087fd3bc4c3e7b105734b1f2
Andy Artz <andyartz@andyartz.com>
2019-12-02 22:26:59 -0600

    Solve Day 2 Part 2

--- src/main/groovy/Day1.groovy
+++ src/main/groovy/Day1.groovy
@@ -10,4 +10,12 @@ class Day1 {
     Integer getRequiredFuel(Collection<Integer> masses) {
         masses.sum { getRequiredFuel(it) } as Integer
     }
+
+    Integer getAllRequiredFuel(Integer mass) {
+        mass < 6 ? 0 : getRequiredFuel(mass).with { it + getAllRequiredFuel(it) }
+    }
+
+    Integer getAllRequiredFuel(Collection<Integer> masses) {
+        masses.sum { getAllRequiredFuel(it) } as Integer
+    }
 }
--- src/test/groovy/Day1Spec.groovy
+++ src/test/groovy/Day1Spec.groovy
@@ -23,12 +23,38 @@ class Day1Spec extends Specification {
         systemUnderTest.getRequiredFuel([12, 14, 1969, 100756]) == 2 + 2 + 654 + 33583
     }
 
-    void "should solve puzzle"() {
+    void "should solve part 1 puzzle"() {
         given:
         List<Integer> masses = getClass().getResource('day1.txt').readLines()*.toInteger()
 
         expect:
         systemUnderTest.getRequiredFuel(masses) == 3324332
+    }
+
+    @Unroll
+    void "should solve total fuel required for mass #mass"() {
+        expect:
+        systemUnderTest.getAllRequiredFuel(mass) == expectedFuel
+
+        where:
+        mass   | expectedFuel
+        12     | 2
+        14     | 2
+        1969   | 966
+        100756 | 50346
+    }
+
+    void "should solve total fuel for a list of masses"() {
+        expect:
+        systemUnderTest.getAllRequiredFuel([12, 14, 1969, 100756]) == 2 + 2 + 966 + 50346
+    }
+
+    void "should solve part 2 puzzle"() {
+        given:
+        List<Integer> masses = getClass().getResource('day1.txt').readLines()*.toInteger()
+
+        expect:
+        systemUnderTest.getAllRequiredFuel(masses) == 4983626
     }
 
 }
