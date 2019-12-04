07d82f55a6d7e0acf802bed0919144c63e9229d7
Andy Artz <andyartz@andyartz.com>
2019-12-03 21:18:43 -0600

    Solves Day 3 Part 1

--- src/test/groovy/Day3.groovy
+++ src/test/groovy/Day3.groovy
@@ -1,5 +1,51 @@
+import math.geom2d.Point2D
+import math.geom2d.line.Line2D
+
 class Day3 {
-    Integer getDistanceToClosestIntersection(List<String> wirePath) {
-        0
+    Integer getDistanceToClosestIntersection(List<String> wirePaths) {
+
+        assert wirePaths.size() == 2
+
+        List<List<Line2D>> wires = wirePaths*.split(',').collect { getWireSegments(it as List) }
+
+        Set<Point2D> intersections = [wires[0], wires[1]].combinations().collectMany { a, b -> getIntersections(a, b) }
+        intersections.removeIf { it == new Point2D(0,0) }
+        intersections.collect { it.x().abs() + it.y().abs() }.min()
+    }
+
+    private List<Line2D> getWireSegments(List<String> wirePathVectors) {
+        List<Line2D> wirePathSegments = []
+
+        wirePathVectors.inject(new Point2D(0, 0)) { previousPoint, pathSegment ->
+            Point2D nextPoint
+            Integer length = pathSegment[1..-1] as Integer
+            String direction = pathSegment[0]
+
+            switch (direction) {
+                case 'U': nextPoint = previousPoint.translate(0, length); break
+                case 'D': nextPoint = previousPoint.translate(0, -length); break
+                case 'L': nextPoint = previousPoint.translate(-length, 0); break
+                case 'R': nextPoint = previousPoint.translate(length, 0); break
+                default: throw new IllegalArgumentException("Expected direction to be U, D, L, or R -- Found $direction")
+            }
+
+            nextPoint.tap {
+                wirePathSegments << new Line2D(previousPoint, it)
+            }
+        }
+
+        wirePathSegments
+    }
+
+    private List<Point2D> getIntersections(Line2D a, Line2D b) {
+        a.intersections(b) ?: a.isColinear(b) ? getColinearIntersections(a, b) : []
+    }
+
+    List<Point2D> getColinearIntersections(Line2D a, Line2D b) {
+        if ([a.x1, a.x2, b.x1, b.x2].unique().size() == 1) {
+            (a.y1.toInteger()..a.y2.toInteger()).intersect((b.y1.toInteger()..b.y2.toInteger())).collect { new Point2D(a.x1, it) }
+        } else {
+            (a.x1.toInteger()..a.x2.toInteger()).intersect((b.x1.toInteger()..b.x2.toInteger())).collect { new Point2D(it, a.y1) }
+        }
     }
 }
--- src/test/groovy/Day3Spec.groovy
+++ src/test/groovy/Day3Spec.groovy
@@ -1,12 +1,23 @@
 import groovy.transform.NotYetImplemented
+import math.geom2d.line.Line2D
 import spock.lang.Specification
 import spock.lang.Unroll
+
 
 class Day3Spec extends Specification {
 
     Day3 systemUnderTest = new Day3()
 
-    @NotYetImplemented
+    void "library should determine intersections and colinearity"() {
+        given:
+        Line2D a = new Line2D(0, 0, 4, 4)
+        Line2D b = new Line2D(3, 3, 8, 8)
+
+        expect:
+        a.isColinear(b)
+        !a.intersections(b)
+    }
+
     @Unroll
     void "should solve for wire path #wirePath"() {
         expect:
@@ -19,12 +30,11 @@ class Day3Spec extends Specification {
         ['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'] | 135
     }
 
-    @NotYetImplemented
     void "should solve puzzle part 1"() {
         given:
-        List<String> wirePath = getClass().getResource('day2.txt').readLines()
+        List<String> wirePath = getClass().getResource('day3.txt').readLines()
 
         expect:
-        systemUnderTest.getDistanceToClosestIntersection(wirePath) == null
+        systemUnderTest.getDistanceToClosestIntersection(wirePath) == 860
     }
 }
